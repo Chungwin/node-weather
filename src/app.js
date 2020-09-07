2a65e9c12fce56ea476699b72cad227f0694ec69
Chung <chung@Chungs-Air.fritz.box>
2020-09-07 21:26:49 +0200

    add about text

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Wather App',
+        title: 'Weather App',
         name: 'Chung Huynh'
     })
 })
